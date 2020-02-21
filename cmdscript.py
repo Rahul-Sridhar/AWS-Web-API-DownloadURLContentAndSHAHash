fb68381abccc1db3bbf49711c8a10e6e324262f7
Rahul-Sridhar <sairahul5223@tamu.edu>
2020-02-21 13:06:51 -0600

    Script to run the web API given input URL

--- cmdscript.py
+++ cmdscript.py
@@ -2,4 +2,4 @@ import requests
 import sys
 
 url = sys.argv[1]
-r = requests.get('https://jc10fl3o4k.execute-api.us-east-2.amazonaws.com/urldownload/url?url='+url)
+r = requests.get(URL_of_AWS_Lambda_API_Gateway+'url?url='+url)
